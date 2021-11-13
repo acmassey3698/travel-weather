0ae5b08e7f1704d22df178c0b51e52af4414a755
Andrew Massey <acmassey@ncsu.edu>
2021-11-13 17:12:08 -0500

    Create error handling for the forecast index

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    coords = ForecastFacade.geocode_city(params[:location])
-    forecast = ForecastFacade.city_weather(coords)
-
+    if params[:location].present?
+      coords = ForecastFacade.geocode_city(params[:location])
+    else
+      return bad_request
+    end
+    if coords.present?
+      forecast = ForecastFacade.city_weather(coords)
+    else
+      return record_not_found
+    end
     render json: ForecastSerializer.city_forecast(forecast)
   end
 end
