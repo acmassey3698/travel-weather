09d37574a8a67e58cdb840c237470fd91c56b560
Andrew Massey <acmassey@ncsu.edu>
2021-11-13 16:12:52 -0500

    Fill in serializer for full response for forecast endpoint

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -18,11 +18,28 @@ class ForecastSerializer
             visibility: forecast.current_weather[:visibility],
             conditions: forecast.current_weather[:conditions],
             icon: forecast.current_weather[:icon]
-          }
+          },
+          "daily_weather": forecast.daily_weather.map do |day|
+            {
+              date: day[:date],
+              sunrise: day[:sunrise],
+              sunset: day[:sunset],
+              max_temp: day[:max_temp],
+              min_temp: day[:min_temp],
+              conditions: day[:conditions],
+              icon: day[:icon]
+            }
+          end,
+          "hourly_weather": forecast.hourly_weather.map do |hour|
+            {
+              time: hour[:time],
+              temperature: hour[:temperature],
+              conditions: hour[:conditions],
+              icon: hour[:icon]
+            }
+          end
         }
       }
     }
-
   end
-
 end
