ff66b2c8e19d7c615d2a0685b9e00eef3b1ab1bd
Andrew Massey <acmassey@ncsu.edu>
2021-11-13 16:13:01 -0500

    Testing update

--- spec/requests/v1/city_weather_request_spec.rb
+++ spec/requests/v1/city_weather_request_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'city Weather request' do
+RSpec.describe 'city weather request' do
   describe 'happy path' do
     it 'gets the weather data', :vcr do
       get '/api/v1/forecast?location=denver,co'
