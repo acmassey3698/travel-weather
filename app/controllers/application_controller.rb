ca117c70274014af55ba194fc4c314c8f358013d
Andrew Massey <acmassey@ncsu.edu>
2021-11-13 17:12:35 -0500

    Write error actions and create error serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def bad_request
+    render json: ErrorSerializer.bad_request, status: 400
+  end
+
+  def record_not_found
+    render json: ErrorSerializer.not_found(params[:location]), status: 404
+  end
 end
