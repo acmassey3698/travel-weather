640a5abe8b1aa4897216a87b44a2e41e92445c9b
Andrew Massey <acmassey@ncsu.edu>
2021-11-13 15:41:43 -0500

    Update forecast controller to talk to the serailizer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    coords = GeocodingService.get_coordinates(params[:location])
-    @weather = WeatherFacade.city_weather(coords)
+    coords = ForecastFacade.geocode_city(params[:location])
+    forecast = ForecastFacade.city_weather(coords)
+
+    render json: ForecastSerializer.city_forecast(forecast)
   end
 end
