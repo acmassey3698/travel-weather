18fecdbd94999bcdf73c4a4fdb7e9e490812606b
Andrew Massey <acmassey@ncsu.edu>
2021-11-14 11:28:13 -0500

    Create happy path test for the image request

---
+++ spec/requests/v1/background_img_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'background image request' do
+  describe 'happy path' do
+    it 'returns an image when the city is provided', :vcr do
+      get '/api/v1/backgrounds?location=Denver,CO'
+
+      expect(response).to be_successful
+      response_body = JSON.parse(response.body, symbolize_names:true)
+
+      image_attributes = response_body[:data][:attributes][:image]
+
+      expect(response_body).to have_key :data
+      expect(response_body[:data][:type]).to eq("image")
+      expect(response_body[:data][:id]).to eq(nil)
+      expect(image_attributes[:location]).to eq('Denver,CO')
+      expect(image_attributes[:image_url]).to be_a(String)
+      expect(image_attributes[:credit][:author]).to be_a(String)
+      expect(image_attributes[:credit][:profile]).to be_a(String)
+    end
+  end
+
+  describe 'sad path' do
+    it 'returns an error when there is not result for the photo search', :vcr do
+      
+    end
+  end
+
+  describe 'edge case' do
+
+  end
+end
