3315e7293b8970ee9a3fe2376746002fc84add7a
Andrew Massey <acmassey@ncsu.edu>
2021-11-14 11:29:34 -0500

    Facade service and poro for the backgrounds controller index action

---
+++ app/facades/backgrounds_facade.rb
@@ -0,0 +1,16 @@
+class BackgroundsFacade
+
+  def self.search_image(location)
+    response = ImageService.search(location)
+    top_result = JSON.parse(response.body, symbolize_names:true)[:results][0]
+
+    attributes = {
+      location: location,
+      image_url: top_result[:urls][:regular],
+      author: top_result[:user][:username],
+      profile: top_result[:user][:links][:self]
+    }
+
+    Image.new(attributes)
+  end
+end
