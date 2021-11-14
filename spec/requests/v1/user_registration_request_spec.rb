4054727a56c83e67804fecb2b5b8229f8a1fe122
Andrew Massey <acmassey@ncsu.edu>
2021-11-14 15:01:09 -0500

    Sad path and edge case testing for user creation

--- spec/requests/v1/user_registration_request_spec.rb
+++ spec/requests/v1/user_registration_request_spec.rb
@@ -23,11 +23,56 @@ RSpec.describe 'user registration endpoint' do
   end
 
   describe 'sad path' do
+    it 'does not authenticate when the user misvalidates their password', :vcr do
+      post '/api/v1/users', params: {
+        email: 'email@email.com',
+        password: 'abc123',
+        password_confirmation: '23'
+      } , as: :json
 
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+
+      response_body = JSON.parse(response.body, symbolize_names:true)
+
+      expect(response_body[:message]).to eq("Unauthorized")
+      expect(response_body[:error]).to eq("Invalid credentials provided")
+    end
+
+    it 'does not allow for two users with the same email', :vcr do
+      user1 = User.create!(email: 'yaknow@mail.com', password: "abc", password_confirmation: "abc")
+
+      post '/api/v1/users', params: {
+        email: 'yaknow@mail.com',
+        password: 'abc123',
+        password_confirmation: 'abc123'
+      } , as: :json
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+
+      response_body = JSON.parse(response.body, symbolize_names:true)
+
+      expect(response_body[:message]).to eq("Unauthorized")
+      expect(response_body[:error]).to eq("Invalid credentials provided")
+    end
   end
 
   describe 'edge case' do
+    it 'does not authenticate when there is no email provided', :vcr do
+      post '/api/v1/users', params: {
+        email: '',
+        password: 'abc123',
+        password_confirmation: 'abc123'
+      } , as: :json
 
+      expect(response).to_not be_successful
+      expect(response.status).to eq(401)
+
+      response_body = JSON.parse(response.body, symbolize_names:true)
+
+      expect(response_body[:message]).to eq("Unauthorized")
+      expect(response_body[:error]).to eq("Invalid credentials provided")
+    end
   end
-
 end
