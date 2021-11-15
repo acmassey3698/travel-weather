ebebbc484cad926c3150434a3ce792ccfb46f029
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 11:54:38 -0500

    Fix typo in controller

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::ActivitiesController < ApplicationController
   def index
     coords    = ForecastFacade.geocode_city(params[:destination])
     forecast  = ForecastFacade.city_weather(coords)
-    activites = ActivitiesFacade.find_activities(forecast)
+    activities = ActivitiesFacade.find_activities(forecast)
   end
 end
