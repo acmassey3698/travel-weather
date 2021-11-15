f597cb507a6f4012a521520806279d984662dda1
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 12:46:27 -0500

    Bulk up happy path testing for the new endpoint

--- spec/requests/v1/activities_request_spec.rb
+++ spec/requests/v1/activities_request_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe 'activity request spec' do
       expect(response_body[:data][:attributes]).to have_key :activities
       expect(response_body[:data][:attributes]).to have_key :destination
       expect(response_body[:data][:attributes]).to have_key :forecast
+
+      expect(response_body[:data][:attributes][:destination]).to eq("Denver,CO")
+      expect(response_body[:data][:attributes][:forecast][:temperature]).to be_a String
+      expect(response_body[:data][:attributes][:forecast][:summary]).to be_a String
+      expect(response_body[:data][:attributes][:activities]).to be_an Array
+
+      response_body[:data][:attributes][:activities].each do |activity|
+        expect(activity[:title]).to be_a String
+        expect(activity[:type]).to be_a String
+        expect(activity[:participants]).to be_a Numeric
+        expect(activity[:price]).to be_a Numeric
+      end
     end
   end
 
