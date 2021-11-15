09eb2ea099fe3b305ec6faf971dc3995ac1b8ab3
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 17:59:20 -0500

    Create method #get_directions in the map service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -2,11 +2,20 @@ class MapService
   def self.conn
     Faraday.new('http://www.mapquestapi.com')
   end
-  
+
   def self.geocode(city_state)
     conn.get('/geocoding/v1/address') do |request|
       request.params['key'] = ENV['MAPQUEST_API_KEY']
       request.params['location'] = city_state
+    end
+  end
+
+  def self.get_directions(origin, destination)
+    conn.get('/directions/v2/route') do |request|
+      request.params['key'] = ENV['MAPQUEST_API_KEY']
+      request.body = {
+        "locations": [origin, destination]
+        }.to_json
     end
   end
 end
