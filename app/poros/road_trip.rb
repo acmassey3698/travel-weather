8d6dbd298a7d77271d0b39c3769ac7efec6d0b3c
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 17:59:38 -0500

    Create poro and its serializer for a roadtrip

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,30 @@
+class RoadTrip
+  attr_reader :start_city,
+              :end_city,
+              :travel_time,
+              :forecast,
+              :weather_at_eta
+
+  def initialize(attrs)
+    @start_city = attrs[:start_city]
+    @end_city = attrs[:end_city]
+    @travel_time = attrs[:travel_time]
+    @weather_at_eta = arrival_weather(attrs[:forecast])
+  end
+
+  def arrival_weather(forecast)
+    hours = travel_time[0..1].to_i - 1
+    if hours < 48
+      forecast.hourly_weather[hours]
+    elsif hours == -1
+      forecast.current_weather
+    else
+      multi_day(hours)
+    end
+  end
+
+  def multi_day(hours)
+    days = (hours/24) - 1
+    forecast.daily_weather[days]
+  end
+end
