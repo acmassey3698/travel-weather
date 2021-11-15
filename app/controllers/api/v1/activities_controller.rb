d16bd375c47f98349d4dca71b1444f95d04032e2
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 11:40:33 -0500

    Build out index action in the activities controller, VCR recording

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ActivitiesController < ApplicationController
 
   def index
-
-  end 
+    coords    = ForecastFacade.geocode_city(params[:destination])
+    forecast  = ForecastFacade.city_weather(coords)
+    activites = ActivitiesFacade.find_activities(forecast)
+  end
 end
