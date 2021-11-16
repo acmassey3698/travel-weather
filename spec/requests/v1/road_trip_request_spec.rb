e1abd10e6582fcb0dc1bd3762d8529d7d143ef04
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 19:02:21 -0500

    New VCR and update testing for request

--- spec/requests/v1/road_trip_request_spec.rb
+++ spec/requests/v1/road_trip_request_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'road trip create request' do
     @user1 = User.create!(
       email: 'java@java.com',
       password: 'java',
-      password_confirmation: 'java'
+      password_confirmation: 'java',
+      api_key: SecureRandom.hex
     )
   end
 
@@ -20,6 +21,15 @@ RSpec.describe 'road trip create request' do
       expect(response).to be_successful
 
       response_body = JSON.parse(response.body, symbolize_names:true)
+
+      expect(response_body).to have_key :data
+      expect(response_body[:data][:id]).to be_falsey
+      expect(response_body[:data][:type]).to eq('roadtrip')
+      expect(response_body[:data][:attributes][:start_city]).to eq('Denver,CO')
+      expect(response_body[:data][:attributes][:end_city]).to eq('Las Vegas,NV')
+      expect(response_body[:data][:attributes][:travel_time]).to be_a String
+      expect(response_body[:data][:attributes][:weather_at_eta][:temperature]).to be_a Numeric
+      expect(response_body[:data][:attributes][:weather_at_eta][:conditions]).to be_a String
     end
 
     it 'does it for a short trip', :vcr do
@@ -32,6 +42,15 @@ RSpec.describe 'road trip create request' do
       expect(response).to be_successful
 
       response_body = JSON.parse(response.body, symbolize_names:true)
+
+      expect(response_body).to have_key :data
+      expect(response_body[:data][:id]).to be_falsey
+      expect(response_body[:data][:type]).to eq('roadtrip')
+      expect(response_body[:data][:attributes][:start_city]).to eq('Charlotte,NC')
+      expect(response_body[:data][:attributes][:end_city]).to eq('Gastonia,NC')
+      expect(response_body[:data][:attributes][:travel_time]).to be_a String
+      expect(response_body[:data][:attributes][:weather_at_eta][:temperature]).to be_a Numeric
+      expect(response_body[:data][:attributes][:weather_at_eta][:conditions]).to be_a String
     end
   end
 
