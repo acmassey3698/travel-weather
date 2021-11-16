5ab93baa35b039a1710596096ab42e92c324f3ac
Andrew Massey <acmassey@ncsu.edu>
2021-11-16 13:20:02 -0500

    Test road trip poro

---
+++ spec/poros/road_trip_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe RoadTrip do
+  describe 'initialize' do
+    it 'exists with attributes', :vcr do
+      result = RoadTripFacade.new_trip('Denver,CO', 'Houston,TX')
+
+      expect(result.start_city).to eq('Denver,CO')
+      expect(result.end_city).to eq('Houston,TX')
+      expect(result.travel_time).to eq("15:38:27")
+      expect(result.weather_at_eta[:temperature]).to eq(68.88)
+      expect(result.weather_at_eta[:conditions]).to eq("clear sky")
+    end
+  end
+
+  describe 'instance methods' do
+    it '#arrival_weather for a very short trip', :vcr do
+      result = RoadTripFacade.new_trip('Charlotte,NC', 'Gastonia,NC')
+
+      expect(result.weather_at_eta[:temperature]).to eq(63.73)
+      expect(result.weather_at_eta[:conditions]).to eq('clear sky')
+    end
+
+    it '#arrival_weather for a very long trip', :vcr do
+      result = RoadTripFacade.new_trip('Charlotte,NC', 'Anchorage,AK')
+
+      expect(result.weather_at_eta[:temperature]).to eq(2.19)
+      expect(result.weather_at_eta[:conditions]).to eq('scattered clouds')
+    end
+  end
+end
