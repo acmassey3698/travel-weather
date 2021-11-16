00fd5dedb04e3a7f2921af5c1057f3e117e6f532
Andrew Massey <acmassey@ncsu.edu>
2021-11-16 15:31:39 -0500

    Beef up facade testing

--- spec/facades/backgrounds_facade_spec.rb
+++ spec/facades/backgrounds_facade_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe BackgroundsFacade do
   describe 'class methods' do
     it '#search_image', :vcr do
-      expect(BackgroundsFacade.search_image("Denver,CO")).to be_a(Image)
+      success = BackgroundsFacade.search_image("Denver,CO")
+      expect(success).to be_a(Image)
+      expect(success.author).to be_a String
+      expect(success.image_url).to be_a String
+      expect(success.location).to be_a String
+      expect(success.profile).to be_a String
       expect(BackgroundsFacade.search_image("aewiorugnbiaowugbi")).to be_falsey
     end
   end
