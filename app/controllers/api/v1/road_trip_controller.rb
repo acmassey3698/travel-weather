56ea73205d9d37075e3ec73f5bf251605c7850e4
Andrew Massey <acmassey@ncsu.edu>
2021-11-15 19:02:35 -0500

    Update controller to validate the api_key

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::RoadTripController < ApplicationController
+  before_action :check_key
+
   def create
     road_trip = RoadTripFacade.new_trip(params[:origin], params[:destination])
     render json: RoadTripSerializer.one_trip(road_trip)
+  end
+
+  private
+  def check_key
+    if params[:api_key].present?
+      validate_key
+    else
+      unauthorized
+    end
+  end
+
+  def validate_key
+    users = User.validate_api_key(params[:api_key])
+    if users.empty?
+      unauthorized
+    end
   end
 end
